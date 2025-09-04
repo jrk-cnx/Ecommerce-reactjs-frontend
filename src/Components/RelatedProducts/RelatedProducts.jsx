e03508b842527d04c1f35fcb7323c6fe9d8fdfb6
WorldAtlas-ReactJS <kujatin74@gmail.com>
2025-09-04 17:24:43 +0530

    change data

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import './RelatedProducts.css'
-import Item from '../Item/Item'
-import data_product from '../../assets/Data'
-
+import React from "react";
+import "./RelatedProducts.css";
+import Item from "../Item/Item";
+import data_product from "../../assets/data";
 
 const RelatedProducts = () => {
   return (
-    <div className='relatedproducts'>
+    <div className="relatedproducts">
       <h1>Related Products</h1>
       <hr />
       <div className="relatedproducts-item">
-        {data_product.map((item, i)=>{
-            return <Item key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price} />
+        {data_product.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RelatedProducts
+export default RelatedProducts;
